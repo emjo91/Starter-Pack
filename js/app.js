a8bc135398901de4e91fcda622d0b811aa0398f5
emjo91 <3mle33@gmail.com>
2018-08-17 10:27:39 -0500

    finished webpack wordpress set up

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
+//Require SASS
+require('../scss/main.scss');
+
+//Require CSS
+require('../style.css');
+
 // Require Scripts
 require('./script');
+
+//Require Slick
+require('script-loader!../node_modules/slick-carousel/slick/slick');
