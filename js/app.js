e2b511a10f9911731ec1cef1df62d05de305f57b
emjo91 <3mle33@gmail.com>
2018-08-21 15:15:50 -0500

    set up dev server

--- js/app.js
+++ js/app.js
@@ -3,4 +3,7 @@ import {top, bottom} from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
-console.log(nav(), top, bottom, makeButton("My first button!"), makeColorStyle("cyan"));
+const button = makeButton("Yay! A button.");
+button.style = makeColorStyle("cyan");
+
+document.body.appendChild(button);
--- js/button.js
+++ js/button.js
@@ -1,5 +1,11 @@
-const makeButton = (buttonName) => {
-	return 'Button: ${buttonName}';
+const makeButton = buttonName => {
+	const buttonLabel = 'Button: ${buttonName}';
+
+	const button = document.createElement("button");
+	button.innerText = buttonLabel;
+
+	return button;
+
 };
 
 module.exports = makeButton;
