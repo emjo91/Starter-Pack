81e1a66801a95f6bc6f658eaf5bdcca73c1c238b
emjo91 <3mle33@gmail.com>
2018-08-23 11:07:15 -0500

    impliemnted HMR and mini css extra plugin

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-
-
 import nav from "./nav";
 import {footer} from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import css from "./footer.css";
 
 const button = makeButton("Yay! A button.");
 button.style = makeColorStyle("#bada55");
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
             entry: "./js/app.js",
             output: {
                 path: __dirname + "/dist",
-                filename: "bundle.js"
+                filename: "bundle.js" 
             },
             plugins: [
                 new HtmlWebpackPlugin(),
